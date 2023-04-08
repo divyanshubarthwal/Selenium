56cf977e1f7d2027f3d3cc0a7235c0378172fdd4
Divyanshu <Divyanshu9927@gmail.com>
2023-04-08 20:55:35 +0530

    change

---
+++ src/Basic/SeleniumIntroduction.java
@@ -0,0 +1,33 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumIntroduction {
+
+	
+	public static void main(String[] args) {
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		/*
+		 System.setProperty("webdriver.chrome.driver","/Users/Divyanshu/chromedriver.exe");
+		  
+		  */
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.quit();
+/*
+ ChromeDriver driver=new ChromeDriver();
+ driver object here has access to all the methods of chrome driver so chrome methods will dont work if we change the browser
+ 
+ WebDriver driver=new ChromeDriver();
+ driver object here has access to the methods of chrome driver which are defiend in web driver interface
+   
+   //webdriver.chrome.driver-> path
+ */
+	}
+
+}
